32ea04800b4b87870a34b53ffd639cac4392c2a0
Alexander Grinvald <grinvald.alexander@gmail.com>
2022-12-14 11:49:46 +0200

    added retry 2 times for cases when no data in response

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
 import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http';
-import { catchError, delay, Observable, throwError } from "rxjs";
+import { catchError, delay, Observable, retry, throwError } from "rxjs";
 import { IProduct } from "../models/product";
 import { ErrorService } from './error.service';
 
@@ -15,16 +15,17 @@ export class ProductsService {
     getAll(): Observable<IProduct[]> {
         //return this.http.get<IProduct[]>('https://fakestoreapi.com/products1', //for error handler showing
         return this.http.get<IProduct[]>('https://fakestoreapi.com/products',
-        {
-            //show only 5 items
-            //params: new HttpParams().append('limit', 5)
-            //or
-            //params: new HttpParams({fromString:'limit=5'})
-            //or
-            params: new HttpParams({ fromObject: { limit: 5 } })
-        }).pipe(delay(1000),//wait 1 second
-            catchError(this.errorHandler.bind(this)) //bind for this context
-        )
+            {
+                //show only 5 items
+                //params: new HttpParams().append('limit', 5)
+                //or
+                //params: new HttpParams({fromString:'limit=5'})
+                //or
+                params: new HttpParams({ fromObject: { limit: 5 } })
+            }).pipe(delay(1000),// wait 1 second
+                retry(2),// send new request if no data in current request 
+                catchError(this.errorHandler.bind(this)) //bind for this context
+            )
     }
 
     private errorHandler(error: HttpErrorResponse) {
